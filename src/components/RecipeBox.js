b2d0487668411e0026b73937509999a07864f8da
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-08-09 11:31:59 -0700

    Add skeleton structure

---
+++ src/components/RecipeBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class RecipeBox extends React.Component {
+    render() {
+        return(
+            <h1>Recipe Box Component</h1>
+        );
+    }
+}
+
+export default RecipeBox;
