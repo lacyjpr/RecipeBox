a6de93ce8f527d50cde0dc1e9a930150a7f2792b
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-08-13 10:11:21 -0700

    Add AddRecipe modal

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,9 +1,23 @@
+//Add modal credit: https://daveceddia.com/open-modal-in-react/
+
 import React from 'react';
+import './AddRecipe.css';
 
 class AddRecipe extends React.Component {
     render() {
+        if (!this.props.show) {
+            return null;
+        }
+
         return(
-            <h1>Add Recipe</h1>
+            <div className="backdrop">
+                <div className="modal">
+                    <h3>Add Recipe</h3>
+                    <div className="footer">
+                        <button onClick={this.props.onClose}>Close</button>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- src/components/RecipeBox.js
+++ src/components/RecipeBox.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import {startLogout} from './../actions/actions';
+import AddRecipe from './AddRecipe';
 
 class RecipeBox extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            addRecipeOpen: false
+        };
 
         this.handleLogout = this.handleLogout.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
@@ -18,7 +23,9 @@ class RecipeBox extends React.Component {
     }
 
     handleAdd() {
-        this.props.history.push('/addrecipe');
+        this.setState({
+            addRecipeOpen: !this.state.addRecipeOpen
+        });
     }
 
     render() {
@@ -27,6 +34,7 @@ class RecipeBox extends React.Component {
                 <h1>Recipe Box Component</h1>
                 <button type='button' onClick={this.handleLogout}>Logout</button>
                 <button type='button' onClick={this.handleAdd}>Add Recipe</button>
+                <AddRecipe show={this.state.addRecipeOpen} onClose={this.handleAdd}/>
             </div>
         );
     }
