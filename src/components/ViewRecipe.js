771ab9062119496d1315ce02bf179d4cee934028
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-08-17 19:08:44 -0700

    Start styling ViewRecipe

--- src/components/ViewRecipe.js
+++ src/components/ViewRecipe.js
@@ -12,16 +12,16 @@ class ViewRecipe extends React.Component{
         }
         
         return(
-        <div className="backdrop">
-            <div className="modal">
+        <div className="view-backdrop">
+            <div className="view-modal">
                 <h3>{this.props.recipeName}</h3>
                 {this.props.imageURL.length > 0 &&
-                   <img src={this.props.imageURL} alt="Serving Suggestion" width={128} height={128}/> 
+                   <img src={this.props.imageURL} className="img" alt="Serving Suggestion" width={128} height={128}/> 
                 }
                 <h4>Ingredients:</h4>
-                <p>{this.props.ingredients}</p>
+                <p className="view-ingredients">{this.props.ingredients}</p>
                 <h4>Directions:</h4>
-                <p>{this.props.directions}</p>
+                <p className="view-directions">{this.props.directions}</p>
                 <button onClick={this.props.onEdit}>Edit</button>
                 <button onClick={() => {
                     dispatch(startDeleteRecipe(this.props.id));
