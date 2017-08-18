99213ca8928138a93d18ed7ffd3aa8454faebb16
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-08-17 22:15:48 -0700

    Add default recipe images

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ import RecipeBox from './components/RecipeBox';
 const store = configure();
 const customHistory = createBrowserHistory();
 
+// Default recipes
+store.dispatch(actions.startAddRecipe('Soy Chicken', 'http://del.h-cdn.co/assets/15/51/1450278988-honey-soy-chicken.jpg', '', ''));
+store.dispatch(actions.startAddRecipe('Filipino Pork Adobo', 'http://salu-salo.com/wp-content/uploads/2015/04/Pork-Adobo-3.jpg', '', ''));
+store.dispatch(actions.startAddRecipe('Peanut Butter & Jelly', 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/7/25/4/FNM_090112-Peanut-Butter-and-Jelly-Sandwich-Cake-Recipe_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382541616148.jpeg', '', ''));
+store.dispatch(actions.startAddRecipe('Tuna Casserole', 'https://blog.bestbullysticks.com/wp-content/uploads/2011/03/tunacas1.jpg', '', ''));
+
 // Protect private routes credit: Tyler McGinnis https://github.com/tylermcginnis/react-router-firebase-auth/blob/master/src/components/index.js
 // Further explanation here: https://stackoverflow.com/questions/43484302/whate-does-it-mean-rest-in-react-jsx
 function PrivateRoute ({component: Component, auth}) {
